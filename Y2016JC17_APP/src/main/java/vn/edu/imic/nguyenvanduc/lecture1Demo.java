0f5c5061946c587abfb49fed586ca42e24bd0bc6
Nguyen Van Duc <nvduc.tk@gmail.com>
2016-03-28 20:49:44 +0700

    hello

--- Y2016JC17_APP/src/main/java/vn/edu/imic/nguyenvanduc/lecture1Demo.java
+++ Y2016JC17_APP/src/main/java/vn/edu/imic/nguyenvanduc/lecture1Demo.java
@@ -3,6 +3,17 @@ package vn.edu.imic.nguyenvanduc;
 public class lecture1Demo {
 public static void main(String[] args)
 {
-	System.out.println("Hello world");
+	int a=5;
+	int b=3;
+	System.out.println("Hello world \n" +(a+b));
+	System.out.println("iMicroSoft - Dạy kinh nghiệm lập trình - Đồ họa đa phương tiện - Bảo mật hệ thống thông tin");
+	System.out.println("Chào mừng các bạn đến với lớp học Java for Base tại iMicrosoft");
+	System.out.println("Đây là ví dụ đơn giản sử dụng phương thức Print trong thư viện");
+	System.out.println(Byte.MAX_VALUE);
+	System.out.println(Byte.MIN_VALUE);
+	System.out.println(Long.MIN_VALUE);
+	System.out.println(Long.MAX_VALUE);
+	System.out.println(Double.MAX_VALUE);
+	
 	}
 }
