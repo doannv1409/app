f7bba252fe6f695e0db4dd52b603ef9fd2ae0cc9
Nguyen Van Duc <nvduc.tk@gmail.com>
2016-03-28 21:37:16 +0700

    hello

--- src/main/java/vn/edu/imic/nguyenvanduc/lecture1Demo.java
+++ src/main/java/vn/edu/imic/nguyenvanduc/lecture1Demo.java
@@ -1,5 +1,27 @@
 package vn.edu.imic.nguyenvanduc;
 
 public class lecture1Demo {
+	public static void main(String[] args)
+	{
+		int a=100;
+		int b=200;
+		int c=a+b;
+		String strString = "iMicroSoft.edu.vn";
+		double numA=20.5;
+		float numB=20.5f;
+		float numC=20.5f;
+		Boolean bFlag1=true;
+		Boolean bFlag2=true;
 
+		System.out.println("Hello world \n" +(a+b));
+		System.out.println("iMicroSoft - Dạy kinh nghiệm lập trình - Đồ họa đa phương tiện - Bảo mật hệ thống thông tin");
+		System.out.println("Chào mừng các bạn đến với lớp học Java for Base tại iMicrosoft");
+		System.out.println("Đây là ví dụ đơn giản sử dụng phương thức Print trong thư viện");
+		System.out.println(""+strString);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		}
 }
