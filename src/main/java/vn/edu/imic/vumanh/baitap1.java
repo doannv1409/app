817185b082d0ef098cdf16d55d013ca551b7895b
vumanh <vumanh-PC>
2016-03-30 19:51:46 +0700

    vumanh

---
+++ src/main/java/vn/edu/imic/vumanh/baitap1.java
@@ -0,0 +1,10 @@
+package vn.edu.imic.vumanh;
+
+public class baitap1 {
+	public static void main(String[] argt) {
+		int a=2;
+		float b=5.6f;
+		sum=float(sqrt(a)*b*b)+(4*a*sqrt(b));
+		System.out.print("c la:");
+	}
+}
