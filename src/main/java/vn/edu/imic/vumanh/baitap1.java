6b81d8902a0f0447427c20406d14badbab962c73
vumanh <vumanh-PC>
2016-03-30 20:04:06 +0700

    vumanh

--- src/main/java/vn/edu/imic/vumanh/baitap1.java
+++ src/main/java/vn/edu/imic/vumanh/baitap1.java
@@ -1,10 +1,10 @@
 package vn.edu.imic.vumanh;
 
 public class baitap1 {
-	public static void main(String[] argt) {
+	public static void main(String[] args) {
 		int a=2;
 		float b=5.6f;
-		sum=float(sqrt(a)*b*b)+(4*a*sqrt(b));
-		System.out.print("c la:");
+		double sum=Math.sqrt(a)*b*b+4*a*Math.sqrt(b);
+		System.out.println("sum la:"+sum);
 	}
 }
