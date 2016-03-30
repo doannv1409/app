74ca73def3147cd387398742f28a3ab88d40e60f
Linh <mr.lynk92@gmail.com>
2016-03-30 20:04:49 +0700

    linh ex1

--- src/main/java/vn/edu/imic/linh/Lecture1.java
+++ src/main/java/vn/edu/imic/linh/Lecture1.java
@@ -4,7 +4,6 @@ public class Lecture1 {
     	    public static void main(String[] args){
     	    	int a =2;
     	    	double b=5.6;
-    	    
-    	    	System.out.println(Math.sqrt(a)*Math.pow(b, 2)+4*a*Math.sqrt(b)); 
+    	        System.out.println(Math.sqrt(a)*Math.pow(b, 2)+4*a*Math.sqrt(b)); 
     	    }
 }
