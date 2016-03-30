5a6fb56572f97a6ed84d90d34d95db1b5357befa
Hieu <Hieu@Hieu-PC>
2016-03-30 19:49:59 +0700

    nguyen hong hieu

--- src/main/java/vn/edu/imic/honghieu/hieu.java
+++ src/main/java/vn/edu/imic/honghieu/hieu.java
@@ -1,5 +1,13 @@
 package vn.edu.imic.honghieu;
 
 public class hieu {
-
+	public static void main (String[] arg)
+	{
+		int a = 2;
+		float b = 5.6f;
+		
+		float sum = c;
+		c = sqrt(a)*b*b+4*a*sqrt(b);
+		System.out.println(c=" " +c);
+	}
 }
