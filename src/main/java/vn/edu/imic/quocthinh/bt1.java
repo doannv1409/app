82a8b2b0055059c301234e187a0681832ed11294
Thinh <Thinh@Thinh-PC>
2016-03-30 19:50:08 +0700

    bt1

---
+++ src/main/java/vn/edu/imic/quocthinh/bt1.java
@@ -0,0 +1,12 @@
+package vn.edu.imic.quocthinh;
+
+public class bt1 {
+	public static void main (String[] arg){
+		int a=2;
+		float b=5.6f;
+		
+		float sum = sqrt(a)*b*b+(4*a*sqrt(b));
+		System.out.println("sum= "+sum);
+		
+	}
+}
