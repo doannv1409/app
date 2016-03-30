e1e15ea7d08ac4c2c8c4b5a380b3735795577adc
Thinh <ngthinh10395@gmail.com>
2016-03-30 20:27:11 +0700

    thinh

--- src/main/java/vn/edu/imic/quocthinh/bt1.java
+++ src/main/java/vn/edu/imic/quocthinh/bt1.java
@@ -1,11 +1,11 @@
 package vn.edu.imic.quocthinh;
 
 public class bt1 {
-	public static void main (String[] arg){
+	public static void main (String[] args){
 		int a=2;
 		float b=5.6f;
 		
-		float sum = sqrt(a)*b*b+(4*a*sqrt(b));
+		double sum = Math.sqrt(a)*b*b+(4*a*Math.sqrt(b));
 		System.out.println("sum= "+sum);
 		
 	}
