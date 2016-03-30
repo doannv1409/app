9ad4eb300b015a7e2f71c1b4dd75d7a8fbce8e71
LeHaiYen <leeyen0894@gmail.com>
2016-03-30 20:34:12 +0700

    bai 1

--- src/main/java/vn/edu/imic/LeHaiYen/BaiTap1.java
+++ src/main/java/vn/edu/imic/LeHaiYen/BaiTap1.java
@@ -2,14 +2,11 @@ package vn.edu.imic.LeHaiYen;
 
 public class BaiTap1 {
 	public static void main(String[] args) {
-	int a=2;
-	float b=(float) 5.6;
-	
-	float can = (float) (Math.sqrt(a)*b*b + 4*a*Math.sqrt(b));
-	System.out.println(can);
+		int a = 2;
+		float b = (float) 5.6;
+
+		float c = (float) (Math.sqrt(a) * b * b + 4 * a * Math.sqrt(b));
+		System.out.println(c);
 	}
-			
-	
-	
 
 }
