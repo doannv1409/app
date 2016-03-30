6ba15ddbb06592467ac3b48ebfbba41b7e32a35f
HOang DAu <hoangnd.cs@gmail.com>
2016-03-30 20:26:55 +0700

    aa

--- src/main/java/vn/edu/imic/hoangnd/Lecture1.java
+++ src/main/java/vn/edu/imic/hoangnd/Lecture1.java
@@ -5,6 +5,6 @@ public class Lecture1 {
 		double a = 2;
 		double b = 5.6;
 		double c = Math.sqrt(a)*b*b + 4*a*Math.sqrt(b);
-		System.out.println("Ket qua: "+c); 
+		System.out.println("Ket qua: "+c);
 	}
 }
