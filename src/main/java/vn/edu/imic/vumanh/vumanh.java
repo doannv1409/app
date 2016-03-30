d9aeba0d8a00cd55dff3f0bba73ec032a668f1ce
vumanh <vumanh-PC>
2016-03-30 19:50:20 +0700

    vumanh

--- src/main/java/vn/edu/imic/vumanh/vumanh.java
+++ src/main/java/vn/edu/imic/vumanh/vumanh.java
@@ -1,5 +1,16 @@
 package vn.edu.imic.vumanh;
 
 public class vumanh {
+	public static void main(String[] argt) {
+		//print say hello
+		System.out.println(" Say hello.");
+		//khai bao biet kieu
+		int a=1;
+		float b= 12.4f;
+		double c= 12.5;
+		
+		
+		
+	}
 
 }
