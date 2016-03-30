83a64303b3065407ad17181922b5c2e65318b1bc
Nguyen Van Duc <nvduc.tk@gmail.com>
2016-03-30 19:44:30 +0700

    hello

---
+++ src/main/java/vn/edu/imic/nguyenvanduc/ex2.java
@@ -0,0 +1,14 @@
+package vn.edu.imic.nguyenvanduc;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+	int a=2;
+	float b=5.6f;
+	
+	
+	float s=(float) ((Math.sqrt(a))*b*b+4*a*(Math.sqrt(b)));
+	System.out.println("Kết quả "+s);
+	}
+
+}
