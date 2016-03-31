60434a48193ea515d9b404df6b804eaf42c62874
trinh <trinhcover@gmail.com>
2016-03-31 16:22:36 +0700

    btvn

---
+++ src/main/java/vn/edu/imic/trinhnv/exercises4.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercises;
+
+/**
+ *
+ * @author trinhcover
+ */
+public class exercises4 {
+    public static void main(String[] args) {
+        //a
+        int a = 10;
+        if (a > 0) {
+            System.out.println("Gia tri cua a > 0");
+        }else{
+            System.out.println("Gia tri cua a < 0");
+        }
+        
+        //b
+        int b = 2;
+        int i = 1;
+         while (i < b) {            
+             System.out.println("gia tri cua i = " + i);
+             i ++;
+        }
+        //c
+        int c = 10;
+        do {            
+            System.out.println("gia tri cua i = " + i);
+            i ++;
+        } while (i < c);
+        
+        //d
+        int d = 10;
+        for (int j = 0; j < d; j++) {
+            System.out.println("gia tri cua j = " + j);
+        }
+    }
+}
