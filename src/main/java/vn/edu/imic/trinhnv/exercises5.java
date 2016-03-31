970d68b1256838b0cb03b5e14a84b4da127b5538
trinh <trinhcover@gmail.com>
2016-03-31 16:42:18 +0700

    ss

--- src/main/java/vn/edu/imic/trinhnv/exercises5.java
+++ src/main/java/vn/edu/imic/trinhnv/exercises5.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exercises;
+package vn.edu.imic.trinhnv;
 
 import java.util.Scanner;
 
