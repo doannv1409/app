aacd93c41b44d68d5c6b108a9049d55d6a94078c
trinh <trinhcover@gmail.com>
2016-03-31 16:54:34 +0700

    new commit

---
+++ src/main/java/vn/edu/imic/trinhnv/exercise1.java
@@ -0,0 +1,11 @@
+package vn.edu.imic.trinhnv;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        System.out.println("iMicrosoft - day kinh nghiem lap trinh- do hoa da phuong tien"
+                + " bao mat he thong thong tin");
+        System.out.println("Chao mung den voi lop hoc java for base tai iMicrosoft");
+        System.out.println("Day la vi du ve print don gian trong thu vien");
+        System.out.println("");
+    }	
+}
---
+++ src/main/java/vn/edu/imic/trinhnv/exercise2.java
@@ -0,0 +1,26 @@
+package vn.edu.imic.trinhnv;
+
+public class exercise2 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int c = a + b;
+        String strString = "iMIC";
+        float numA = 20.5f , numB = 20.5f, 
+                numC = 20.5f, numD = 20.5f;
+        
+        boolean bFlag1 = true;
+        boolean bFlag2 = false;
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + (a + b));
+        System.out.println(strString);
+        System.out.println("numA = " + numA);
+        System.out.println("numB = " + numB);
+        System.out.println("numC = " + numC);
+        System.out.println("numD = " + numD);
+        System.out.println("bFlag1 = " + bFlag1);
+        System.out.println("bFlag2 = " + bFlag2);
+    }
+}
