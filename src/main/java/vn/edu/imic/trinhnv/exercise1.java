122ae5813d4dd9043114a680c39ad78f3dc22cb3
trinh <trinhcover@gmail.com>
2016-04-04 19:38:24 +0700

    my cm

--- src/main/java/vn/edu/imic/trinhnv/exercise1.java
+++ src/main/java/vn/edu/imic/trinhnv/exercise1.java
@@ -1,11 +1,55 @@
 package vn.edu.imic.trinhnv;
 
+import java.util.Scanner;
+
 public class exercise1 {
     public static void main(String[] args) {
-        System.out.println("iMicrosoft - day kinh nghiem lap trinh- do hoa da phuong tien"
-                + " bao mat he thong thong tin");
-        System.out.println("Chao mung den voi lop hoc java for base tai iMicrosoft");
-        System.out.println("Day la vi du ve print don gian trong thu vien");
-        System.out.println("");
-    }	
+    	System.out.println("Moi ban nhap vao hai so");
+    	float A = 0, B = 0;
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Nhap a: ");
+    	String a = sc.nextLine();
+    	if (a == "" || a.isEmpty()) {
+			System.out.println("Ban chua nhap a !");
+		}else {
+			A = Float.parseFloat(a);
+		}
+    	
+    	System.out.print("Nhap b: ");
+    	String b = sc.nextLine();
+    	if (b == "" || b.isEmpty()) {
+			System.out.println("Ban chua nhap b !");
+		}else {
+			B = Float.parseFloat(b);
+		}
+    	
+    	System.out.println("Vui long chon phep tinh 0(+), 1(-), 2(*), 3(/): ");
+       	String choice = sc.nextLine();
+    	if (choice == "" && choice.isEmpty() ) {
+			System.out.println("Ban chua chon phep toan !");
+		}
+    	switch (choice) {
+		case "0":
+			System.out.println("a + b = " + (A + B));
+			break;
+		case "1":
+			System.out.println("a - b = " + (A - B));
+			break;
+		case "2":
+			System.out.println("a * b = " + (A * B));
+			break;
+		case "3":
+				if (A != 0 && B == 0) {
+					System.out.println("Cannot divide by zero");
+				}else if (A == 0 & B == 0) {
+					System.out.println("Result of function is underfined");
+				}
+			System.out.println("a / b = " + (A / B));
+			break;
+		default:
+			System.err.println("Invalid option selected");
+    	}
+    	
+    	sc.close();
+    }
 }
