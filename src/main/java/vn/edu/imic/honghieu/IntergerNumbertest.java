c932b6ed96710561781497bda23bc5d2c269f7c5
Hieu <nguyenhonghieu28195@gmail.com>
2016-04-08 21:07:01 +0700

    hieu

---
+++ src/main/java/vn/edu/imic/honghieu/IntergerNumbertest.java
@@ -0,0 +1,11 @@
+package vn.edu.imic.honghieu;
+
+public class IntergerNumbertest {
+	public static void main (String[] args){
+		IntergerNumber number1 = new IntergerNumber(2);
+		IntergerNumber number2 = new IntergerNumber(3);
+		}
+	private static void sum (IntergerNumber number){
+		
+		}
+}
