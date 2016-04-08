3537608fbb360f4da9c3abbd30e232ea5d35c493
HOang DAu <hoangnd.cs@gmail.com>
2016-04-08 21:24:18 +0700

    aaa

--- src/main/java/vn/edu/imic/hoangnd/IntegerNumberTest.java
+++ src/main/java/vn/edu/imic/hoangnd/IntegerNumberTest.java
@@ -8,14 +8,16 @@ public class IntegerNumberTest {
 //			num2.va
 //			return (num1+num2);
 //		}
-		public void sum(IntegerNumber num1, IntegerNumber num2){
-			return num1+num2;
-		}
+		
 		IntegerNumber number1 = new IntegerNumber();
 		number1.value = 1;
 		IntegerNumber number2 = new IntegerNumber();
 		number2.value = 2;
-		sum(number1, number2);
-		System.out.println(sum);
+		System.out.println(sum(number1, number2).value);
+	}
+	public static IntegerNumber sum(IntegerNumber num1, IntegerNumber num2){
+		IntegerNumber num3 = new IntegerNumber();
+		num3.value = num1.value + num2.value;
+		return num3;
 	}
 }
