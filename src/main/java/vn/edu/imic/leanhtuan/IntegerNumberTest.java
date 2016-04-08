50ffb92f853672407f922f7fb5ba5b868934aa00
team1ecoit <team1ecoit@gmail.com>
2016-04-08 21:07:16 +0700

    class

---
+++ src/main/java/vn/edu/imic/leanhtuan/IntegerNumberTest.java
@@ -0,0 +1,16 @@
+package vn.edu.imic.leanhtuan;
+
+public class IntegerNumberTest {
+		public static void main (String[] args){
+			IntegerNumber number1 = new IntegerNumber(10);
+			IntegerNumber number2 = new IntegerNumber(20);
+			
+			printSum(number1);
+		}
+		
+		static IntegerNumber printSum(IntegerNumber number){
+			System.out.println(number.getIntegerNumber(0));
+			return number;
+			
+		}
+}
