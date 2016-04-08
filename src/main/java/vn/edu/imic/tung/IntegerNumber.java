169d0dc329d934ffa7955457933377b5e61b5fc9
tung <hoangtungdt12k54@gmail.com>
2016-04-08 21:07:50 +0700

    exam

---
+++ src/main/java/vn/edu/imic/tung/IntegerNumber.java
@@ -0,0 +1,10 @@
+package vn.edu.imic.tung;
+
+public class IntegerNumber {
+	public IntegerNumber(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int value;
+
+}
---
+++ src/main/java/vn/edu/imic/tung/IntegerNumberTest.java
@@ -0,0 +1,13 @@
+package vn.edu.imic.tung;
+
+public class IntegerNumberTest {
+	public static void main(String[] args){
+		
+		IntegerNumber obj = new IntegerNumber(6, 4);
+		public  IntegerNumber sum(IntegerNumber number1,IntegerNumber number2 ){
+			
+		};
+		
+	}
+
+}
