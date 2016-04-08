0e76bf8922d8c59df176831038ba3213a0fd1c3c
trinh <trinhcover@gmail.com>
2016-04-08 21:07:25 +0700

    dd

---
+++ src/main/java/vn/edu/imic/trinhnv/IntegerNumber.java
@@ -0,0 +1,16 @@
+package vn.edu.imic.trinhnv;
+
+public class IntegerNumber {
+	private int value;
+	
+	public IntegerNumber(){
+		
+	}
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
---
+++ src/main/java/vn/edu/imic/trinhnv/IntegerNumberTest.java
@@ -0,0 +1,15 @@
+package vn.edu.imic.trinhnv;
+
+public class IntegerNumberTest {
+	public static void main(String[] args) {
+	IntegerNumber n1 = new IntegerNumber();
+	IntegerNumber n2 = new IntegerNumber();
+	n1.setValue(2);
+	n2.setValue(3);
+	
+	}
+	public static int sum (IntegerNumber n1, IntegerNumber n2){
+		
+	}
+}
+
