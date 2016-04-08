30154202b7fde403315461023261773771e5c67b
minhtq <minhtoquang.it@gmail.com>
2016-04-08 21:33:19 +0700

    comment IntegerNumber class

--- src/main/java/vn/edu/imic/hoangnd/IntegerNumberTest.java
+++ src/main/java/vn/edu/imic/hoangnd/IntegerNumberTest.java
@@ -8,16 +8,20 @@ public class IntegerNumberTest {
 //			num2.va
 //			return (num1+num2);
 //		}
-		
 		IntegerNumber number1 = new IntegerNumber();
 		number1.value = 1;
 		IntegerNumber number2 = new IntegerNumber();
 		number2.value = 2;
-		System.out.println(sum(number1, number2).value);
+		
+		IntegerNumber result = sum(number1, number2);
+		
+		System.out.println("Tong 2 so: " + result.value);
 	}
-	public static IntegerNumber sum(IntegerNumber num1, IntegerNumber num2){
-		IntegerNumber num3 = new IntegerNumber();
-		num3.value = num1.value + num2.value;
-		return num3;
+	public static IntegerNumber sum(IntegerNumber num1, 
+			IntegerNumber num2){
+		IntegerNumber result = new IntegerNumber();
+		result.value = num1.value + num2.value;
+		return result;
 	}
+	
 }
--- src/main/java/vn/edu/imic/leanhtuan/IntegerNumber.java
+++ src/main/java/vn/edu/imic/leanhtuan/IntegerNumber.java
@@ -7,10 +7,12 @@ public class IntegerNumber {
 		this.value = value;
 	}
 	
+	//nen de la setValue
 	public void setIntegerNumber(int value){
 		this.value = value;
 	}
 	
+	//nen de getValue ko co params
 	public int getIntegerNumber(int value){
 		return this.value;
 		
