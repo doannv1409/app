0a8f92f9d35376b6f37895c832d34a6d43dab22a
Tung Lam <ptlsm.94@gmail.com>
2016-04-08 09:07:03 +0700

    a

---
+++ src/main/java/vn/edu/imic/tunglam/IntegerNumber.java
@@ -0,0 +1,12 @@
+package vn.edu.imic.tunglam;
+
+public class IntegerNumber{
+
+
+private int value;
+public IntegerNumber(int value){
+	this.value = value;
+}
+
+
+}
---
+++ src/main/java/vn/edu/imic/tunglam/SumInteger.java
@@ -0,0 +1,7 @@
+package vn.edu.imic.tunglam;
+
+public class SumInteger {
+public static void main(String[] args){
+	IntegerNumber number1= new IntegerNumber(value);
+}
+}
