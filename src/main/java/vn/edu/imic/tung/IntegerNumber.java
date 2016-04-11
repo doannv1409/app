a727dc2b0e9cc09872756c358a88184e51334ca8
tung <hoangtungdt12k54@gmail.com>
2016-04-11 19:58:10 +0700

    btvn

--- src/main/java/vn/edu/imic/tung/IntegerNumber.java
+++ src/main/java/vn/edu/imic/tung/IntegerNumber.java
@@ -1,10 +1,7 @@
 package vn.edu.imic.tung;
 
 public class IntegerNumber {
-	public IntegerNumber(int i, int j) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public int value;
+	private int value;
+	public IntegerNumber(){}
 
 }
