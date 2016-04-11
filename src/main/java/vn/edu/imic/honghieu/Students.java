46917f9488b624020a18b6c3173136287a1e9a51
Hieu <nguyenhonghieu28195@gmail.com>
2016-04-11 19:46:43 +0700

    hieu

--- src/main/java/vn/edu/imic/honghieu/Students.java
+++ src/main/java/vn/edu/imic/honghieu/Students.java
@@ -35,4 +35,21 @@ public class Students {
 	public String getphone(){
 		return this.phone;
 	}
+		public static void main (String[] args){
+			Students sv1 = new Students("sv01","  HoangVanThanh","  thanh@gmail.com","  0988123456");
+			print(sv1);
+			Students sv2 = new Students("sv02","  PhamThiThuyLinh","  linh@gmail.com","  0988123457");
+			print(sv2);
+			Students sv3 = new Students("sv03","  TranVanHieu","  hieu@gmail.com","  0988123458");
+			print(sv3);
+			Students sv4 = new Students("sv04","  NguyenVanHung","  hung@gmail.com","  0988123459");
+			print(sv4);
+		}
+
+		private static void print(Students students) {
+			System.out.println(students.getma()+students.getten()+students.getgmail()+students.getphone());
+		}
+
 }
+
+
