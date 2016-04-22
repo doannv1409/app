36b1547dc05381f4e0d49d83d400c99d951d5012
team1ecoit <team1ecoit@gmail.com>
2016-04-22 20:49:50 +0700

    a

--- src/main/java/vn/edu/imic/haiyen/HDT_NickleDime.java
+++ src/main/java/vn/edu/imic/haiyen/HDT_NickleDime.java
@@ -6,15 +6,17 @@ public class HDT_NickleDime extends HDT_Account {
 
 	@Override
 	protected void endMonth() {
-		double count = this.endMonthCharge();
-		this.balance -= count;
-		System.out.println("So tien con lai trong tai khoan: " + this.balance);
-		System.out.println("Tong so giao dich: " + this.withdrawCount);
+//		double count = this.endMonthCharge();
+//		this.balance -= count;
+//		System.out.println("So tien con lai trong tai khoan: " + this.balance);
+//		System.out.println("Tong so giao dich: " + this.withdrawCount);
+		super.endMonth();
+		//Reset count withdraw here
 	};
 
 	@Override
 	protected double endMonthCharge() {
-		// TODO Auto-generated method stub
+		// TODO set withdrawCount ve 0
 		double fee =  this.withdrawCount * FEE;
 		//this.withdrawCount=0;
 		return fee;
@@ -22,7 +24,7 @@ public class HDT_NickleDime extends HDT_Account {
 
 	@Override
 	protected void withdraw(double amount) {
-		this.balance -= amount;// amount >0
+		super.withdraw(amount);
 		withdrawCount++;
 
 	}
