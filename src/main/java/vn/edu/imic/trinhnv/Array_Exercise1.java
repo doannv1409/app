ade368d42b41ae001dfbb902c1a5a81a115cd142
trinhcover <trinhcover@gmail.com>
2016-04-25 19:28:58 +0700

    new arr

---
+++ src/main/java/vn/edu/imic/trinhnv/Array_Exercise1.java
@@ -0,0 +1,33 @@
+package vn.edu.imic.trinhnv;
+
+import java.util.Scanner;
+
+public class Array_Exercise1 {
+	public static void main(String[] args) {
+		int n;
+		int sum = 0;
+		int max = 0;
+		Scanner input = new Scanner(System.in);
+	    System.out.print("Enter the number of elements array: ");
+	    n = input.nextInt();
+		int array[] = new int[n];
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print("Enter elements "+ (i + 1) +" of array : " );
+			array[i] = input.nextInt();
+		}
+	    input.close();
+	    
+	    System.out.println("Each elements of this array is: ");
+	    for (int i = 0; i < array.length; i++) {
+	    	sum += array[i];
+	    	if (max < array[i]) {
+				max = array[i];
+			}
+			System.out.println(array[i]);
+		}
+
+	    System.out.println("Sum all elements of this array is: " + sum);
+	    System.out.println("The biggest element in this array is: " + max);
+	}
+}
--- src/main/java/vn/edu/imic/trinhnv/Gambler.java
+++ src/main/java/vn/edu/imic/trinhnv/Gambler.java
@@ -10,8 +10,7 @@ class Gambler extends Account{
 		Random rand = new Random();
 		int  n = rand.nextInt(100) + 1;
 		//TODO ko tru tien
-		if (n < 49) {
-			this.balance -= amount;
+		if (n <= 49) { 
 			System.out.println("You are so lucky !");
 		}else {
 			this.balance -= (2 * amount);
