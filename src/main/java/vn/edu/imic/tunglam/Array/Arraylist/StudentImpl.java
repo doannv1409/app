47069696429ab71718a56e142fec879d34aa9ba3
Tung Lam <ptlsm.94@gmail.com>
2016-04-27 21:52:03 +0700

    a

--- src/main/java/vn/edu/imic/tunglam/Array/Arraylist/StudentImpl.java
+++ src/main/java/vn/edu/imic/tunglam/Array/Arraylist/StudentImpl.java
@@ -1,7 +1,6 @@
 package vn.edu.imic.tunglam.Array.Arraylist;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,9 +12,9 @@ public class StudentImpl implements StudentManager {
 
 	@Override
 	public void sortByName(List<Student> students) {
-		Collections.sort(students, new Comparator<Student>(){
+		Collections.sort(students, new Comparator<Student>() {
 			@Override
-			public int compare (Student s1, Student s2){
+			public int compare(Student s1, Student s2) {
 				return (s1.getName().compareTo(s2.getName()));
 			}
 		});
@@ -23,7 +22,7 @@ public class StudentImpl implements StudentManager {
 
 	@Override
 	public List<Student> filterByMark(List<Student> students) {
-		List<Student> B = new ArrayList();
+		List<Student> B = new ArrayList<Student>();
 		for (int i = 0; i < students.size() - 1; i++) {
 			if (students.get(i).getMark() >= 8) {
 				B.add(students.get(i));
