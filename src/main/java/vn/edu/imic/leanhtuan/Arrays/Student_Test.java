6900376899c249a1b8cc32a1189f8353bba4f2ed
team1ecoit <team1ecoit@gmail.com>
2016-04-27 19:43:03 +0700

    comit

--- src/main/java/vn/edu/imic/leanhtuan/Arrays/Student_Test.java
+++ src/main/java/vn/edu/imic/leanhtuan/Arrays/Student_Test.java
@@ -13,16 +13,16 @@ public class Student_Test extends Student {
 
 	public static void main(String[] args) {
 
-		Student student1 = new Student("SV01", "Nguyen Van 1", "HN", 9.6);
-		Student student2 = new Student("SV02", "Nguyen Van 2", "HB", 8.6);
-		Student student3 = new Student("SV03", "Nguyen Van 3", "HA", 7.6);
-		Student student4 = new Student("SV04", "Nguyen Van 4", "HC", 7.6);
-		Student student5 = new Student("SV05", "Nguyen Van 5", "HD", 7.6);
-		Student student6 = new Student("SV06", "Nguyen Van 6", "HE", 9.6);
-		Student student7 = new Student("SV07", "Nguyen Van 7", "HF", 7.6);
-		Student student8 = new Student("SV08", "Nguyen Van 8", "HG", 7.6);
-		Student student9 = new Student("SV09", "Nguyen Van 9", "HK", 8.6);
-		Student student10 = new Student("SV10", "Nguyen Van 10", "HI", 7.6);
+		Student student1 = new Student("SV01", "Nguyen Van A", "HN", 9.6);
+		Student student2 = new Student("SV02", "Nguyen Van B", "HB", 8.6);
+		Student student3 = new Student("SV03", "Nguyen Van C", "HA", 7.6);
+		Student student4 = new Student("SV04", "Nguyen Van E", "HC", 7.6);
+		Student student5 = new Student("SV05", "Nguyen Van D", "HD", 7.6);
+		Student student6 = new Student("SV06", "Nguyen Van F", "HE", 9.6);
+		Student student7 = new Student("SV07", "Nguyen Van Z", "HF", 7.6);
+		Student student8 = new Student("SV08", "Nguyen Van H", "HG", 7.6);
+		Student student9 = new Student("SV09", "Nguyen Van I", "HK", 8.6);
+		Student student10 = new Student("SV10", "Nguyen Van K", "HI", 7.6);
 
 		List<Student> list = new ArrayList<>();
 		list.add(student1);
@@ -38,18 +38,19 @@ public class Student_Test extends Student {
 		
 		
 		print(list);
+		System.out.println("-------------------------");
+		printAlpha(list);
+		
+		Collections.sort(list, new Comparator<Student>() {
+			public int compare(Student svA, Student svB) {
+				return svB.getName().compareTo(svA.getName());
+			};
+		});
+
 
 	}
 
-//	public static void printAlphabe(List<Student> list) {
-//		for (int i = 0; i <= list.size(); i++) {
-//
-//			System.out.println("");
-//		}
-//	}
-
 	public static void print(List<Student> list) {
-
 		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i).getAverageMark() >= 8) {
 				System.out.println("-----------" + i + "------------");
@@ -60,13 +61,17 @@ public class Student_Test extends Student {
 				System.out.println("Diem Trung Binh : " + list.get(i).getAverageMark());
 			}
 		}
-
-		Collections.sort(list, new Comparator<Student>() {
-			public int compare(Student svA, Student svB) {
-				return svB.getName().compareTo(svA.getName());
-			};
-		});
-//		System.out.println("Sap Xep Theo Thu Tu Alphabe");
-//		print(list);
+	}
+	
+	public static void printAlpha(List<Student> list){
+		for(int i = 0; i < list.size(); i++){
+			System.out.println("-----------" + i + "------------");
+			System.out.println("Thong Tin Cua Sinh Vien ");
+			System.out.println("Ma So Sinh Vien : " + list.get(i).getId());
+			System.out.println("Ho Ten Sinh Vien : " + list.get(i).getName());
+			System.out.println("Dia Chi Sinh Vien :" + list.get(i).getAddress());
+			System.out.println("Diem Trung Binh : " + list.get(i).getAverageMark());
+		
+		}
 	}
 }
