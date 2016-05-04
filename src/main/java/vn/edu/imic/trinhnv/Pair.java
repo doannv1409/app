32e4a5e646ac2434cda7a0a15702a897ad83203b
trinhcover <trinhcover@gmail.com>
2016-05-04 21:38:33 +0700

    my cm

---
+++ src/main/java/vn/edu/imic/trinhnv/Pair.java
@@ -0,0 +1,35 @@
+package vn.edu.imic.trinhnv;
+
+public class Pair <T> {
+	public Pair(){
+		first = null; 
+		second = null;
+	}
+	public Pair (T first, T second){
+		this.first = first; 
+		this.second = second; 
+	}
+	
+	public T getFirst() {
+		return first; 
+	}
+	public T getSecond() {
+		return second; 
+	}
+	
+	public void setFirst(T newValue) {
+		first = newValue; 
+	}
+	
+	public void setSecond(T newValue) {
+		second = newValue; 
+	}
+	
+	private T first;
+	private T second;
+	
+	@Override
+	public String toString() {
+		return "Pair [first=" + first + ", second=" + second + "]";
+	}
+}
---
+++ src/main/java/vn/edu/imic/trinhnv/TestPair.java
@@ -0,0 +1,8 @@
+package vn.edu.imic.trinhnv;
+
+public class TestPair {
+	public static void main(String[] args) {
+		Pair<String> mm = new Pair<String>("1st", "2nd");
+		System.out.println(mm.getFirst() + "," + mm.getSecond());
+	}
+}
