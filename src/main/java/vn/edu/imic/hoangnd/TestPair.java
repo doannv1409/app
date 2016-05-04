2f3abe01dde20f688c5d972b5b6bd15345e11deb
HOang DAu <hoangnd.cs@gmail.com>
2016-05-04 21:30:08 +0700

    a

---
+++ src/main/java/vn/edu/imic/hoangnd/TestPair.java
@@ -0,0 +1,11 @@
+package vn.edu.imic.hoangnd;
+
+public class TestPair {
+	public static void main(String[] args) {
+		Pair<String> str = new Pair<String>("1st", "2nd");
+		System.out.println("test: " + str.toString());
+		
+		Pair<Integer> intt = new Pair<Integer>(1, 2);
+		System.out.println("test: " + intt.toString());
+	}
+}
