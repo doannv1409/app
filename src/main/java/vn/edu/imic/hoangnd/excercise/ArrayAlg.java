e4d2b54a01cbcc00ce086fe9e11684ec7f632de7
HOang DAu <hoangnd.cs@gmail.com>
2016-05-06 18:56:26 +0700

    â

---
+++ src/main/java/vn/edu/imic/hoangnd/excercise/ArrayAlg.java
@@ -0,0 +1,18 @@
+package vn.edu.imic.hoangnd.excercise;
+
+public class ArrayAlg<T> {
+	private T t;
+
+	public T getT() {
+		return t;
+	}
+
+	public void setT(T t) {
+		t = t;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
