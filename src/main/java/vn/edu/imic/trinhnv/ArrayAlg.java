f861ef47f9c09564255635410d5655ad518f0715
trinhcover <trinhcover@gmail.com>
2016-05-08 15:56:19 +0700

    commit

--- src/main/java/vn/edu/imic/trinhnv/ArrayAlg.java
+++ src/main/java/vn/edu/imic/trinhnv/ArrayAlg.java
@@ -1,36 +1,32 @@
 package vn.edu.imic.trinhnv;
 
+import java.util.Arrays;
+
 public class ArrayAlg <T>{
-	private T a;
-	
-	public ArrayAlg(T arr){
-		this.a = arr;
-	}
-	public <T> void print(T[]a){
+
+	public void print(T[]a){
 		for (T t : a) {
-			System.out.print(t + " ");
+			System.out.print("%s" + t);
 		}
 		System.out.println(" ");
 	}
 	
 	public static <T> void print(T[] a, int start, int end){
-		if (start >= end) {
+		if (start >= end || start < 0 || end > a.length) {
 			throw new RuntimeException("Invalid start or end !");
 		}else {
-			for (int i = start; i <= end; i++) {
-				System.out.print(a[i] + " ");
+			for (int i = start; i < end; i++) {
+				System.out.print("%s" + a[i]);
 			}
-			System.out.println(" ");
+			System.out.println("Elements will be print: " +(end - start));
 		}
 	}
 	
-	public <T extends Comparable<T>>void maxTwo(T[] a){
-		T max = a[0];
-		for(T t : a){
-			if (t.compareTo(max) > 0) {
-				max = t;
-			}
-		}
-		System.out.println(max);
+	public Pair<T> maxTwo(T [] a){
+		Pair<T> result = new Pair<T>();
+		Arrays.sort(a);
+		result.setFirst(a[a.length - 1]);
+		result.setSecond(a[a.length - 2]);
+		return result;
 	}
 }
